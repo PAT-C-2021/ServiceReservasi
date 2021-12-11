317153b89bc624db620b56c473640014f00f6785
YogaAdityaPrabowo-TI <yoga.aditya.ft19@mail.umy.ac.id>
2021-12-12 02:28:53 +0700

    ServiceReservasi-053

--- ServiceReservasi/IService1.cs
+++ ServiceReservasi/IService1.cs
@@ -43,15 +43,15 @@ namespace ServiceReservasi
         [DataMember]
         public int JumlahPemesanan { get; set; }
         [DataMember]
-        public string IDLokasi { get; set; }
+        public string Lokasi { get; set; }
     }
     [ServiceContract]
     public interface IService1
     {
         [OperationContract]
-        string pemesanan(string IDPemesanan, string NamaCustomer, string NoTelfon, int JumlahPemesanan, string IDLokasi);
+        string pemesanan(string IDPemesanan, string NamaCustomer, string NoTelpon, int JumlahPemesanan, string IDLokasi);
         [OperationContract]
-        string editPemesanan(string IDPemesanan, string NamaCustomer);
+        string editPemesanan(string IDPemesanan, string NamaCustomer, string No_telpon);
         [OperationContract]
         string deletePemesanan(string IDPemesanan);
         [OperationContract]
